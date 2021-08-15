c39aa00eda0410e5195bcc943a5394bc126cf3bd
sirtau <contact@hamidmousa.com>
2021-08-15 16:06:51 +1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+import Phaser from "phaser";
+
+let gameScene = new Phaser.Scene('Game');
+
+
+let config = {
+  type: Phaser.AUTO, 
+  width: 640,
+  height: 360,
+  scene: gameScene
+};
+
+
+let game = new Phaser.Game(config);
