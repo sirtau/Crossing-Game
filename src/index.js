c2f2d4603e55755a7a5c6ec7c0a8abb86194ce6c
sirtau <contact@hamidmousa.com>
2021-08-15 16:34:49 +1000

    Loaded assets
    Placed background and player sprite.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-
 import Phaser from "phaser";
 
-let gameScene = new Phaser.Scene('Game');
+let gameScene = new Phaser.Scene("Game");
 
-
-let config = {
-  type: Phaser.AUTO, 
-  width: 640,
-  height: 360,
-  scene: gameScene
+gameScene.preload = function () {
+    this.load.image("background", "../assets/background.png");
+    this.load.image("dragon", "../assets/dragon.png");
+    this.load.image("player", "../assets/player.png");
+    this.load.image("treasure", "../assets/treasure.png");
 };
 
+gameScene.create = function () {
+    let bg = this.add.sprite(0, 0, "background");
+    bg.setOrigin(0,0)
+    let player = this.add.sprite(70, 180, 'player');
+
+    
+    let gameW = this.sys.game.config.width
+    let gameH = this.sys.game.config.height
+};
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 360,
+    scene: gameScene,
+};
 
 let game = new Phaser.Game(config);
