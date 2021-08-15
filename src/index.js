09e3843e6ce3b5c7f765847d07022d7259e37745
sirtau <contact@hamidmousa.com>
2021-08-15 17:21:35 +1000

    Added init and update functions, collision boxes and test Win trigger

--- src/index.js
+++ src/index.js
@@ -4,23 +4,56 @@ let gameScene = new Phaser.Scene("Game");
 
 gameScene.preload = function () {
     this.load.image("background", "../assets/background.png");
-    this.load.image("dragon", "../assets/dragon.png");
+    this.load.image("enemy", "../assets/dragon.png");
     this.load.image("player", "../assets/player.png");
     this.load.image("treasure", "../assets/treasure.png");
 };
 
-gameScene.create = function () {
-    let bg = this.add.sprite(0, 0, "background");
-    bg.setOrigin(0,0)
-    let player = this.add.sprite(70, 180, 'player');
 
+gameScene.init = function() {
+    this.playerSpeed = 2
+}
+
+gameScene.create = function () {
+    this.bg = this.add.sprite(0, 0, "background")
+        .setOrigin(0,0)
+
+    this.player = this.add.sprite(40, 180, 'player')
+        .setScale(.5)
+
+    this.goal = this.add.sprite(config.width - 80, config.height/2, 'treasure')
+        .setScale(.5)
+
+    this.enemy1 = this.add.sprite(250, 180, 'enemy')
+        .setScale(.5)
+
+    this.enemy2 = this.add.sprite(450, 180, 'enemy')
+        .setScale(.5)
     
-    let gameW = this.sys.game.config.width
-    let gameH = this.sys.game.config.height
 };
+
+gameScene.update = function() {
+
+    if(this.input.activePointer.isDown) {
+        this.player.x += this.playerSpeed
+    }
+
+
+    let playerRect = this.player.getBounds()
+    let goalRect = this.goal.getBounds()
+
+
+    if(Phaser.Geom.Intersects.RectangleToRectangle(playerRect, goalRect)) {
+        this.scene.restart()
+    }
+    
+    
+
+}
 
 let config = {
     type: Phaser.AUTO,
+    pixelArt: true,
     width: 640,
     height: 360,
     scene: gameScene,
